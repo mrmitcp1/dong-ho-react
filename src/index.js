628510f34c6fdbee29e134d4e49340a496e8f004
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-05 16:07:09 +0700

    clock

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const stick =() =>{
+    root.render(
+  <div>
+      <h1>Date</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+  </div>
 );
+}
+setInterval(stick,1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
